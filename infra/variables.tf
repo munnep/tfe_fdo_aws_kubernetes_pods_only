b6e63c7537ec4248e73b437e4f25f19e5b546727
munnep <patrick.munne@hashicorp.com>
2025-10-06 11:36:36 +0200

    some small typos

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,29 +10,13 @@ variable "vpc_cidr" {
   description = "which private subnet do you want to use for the VPC. Subnet mask of /16"
 }
 
-variable "rds_password" {
-  description = "password for the RDS postgres database user"
-}
 
 variable "k8s_min_size" {
   description = "Kubernetes nodes minimal size"
   default     = 1
 }
 
-variable "redis_port" {
-  description = "port for redis to listen on"
-  default     = 6380
-}
 
-variable "redis_tls_enabled" {
-  description = "redis tls enabled"
-  default     = true
-}
-
-variable "redis_password" {
-  description = "password for the redis database"
-  default     = "redis-password-secure"
-}
 
 
 variable "k8s_max_size" {
